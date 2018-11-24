96a8237974b825a84cb0d9383262365a76e974b7
David Oakley <david@astraware.com>
2018-11-24 17:29:34 +0000

    Added faction systems list

--- commands/cmd_faction.js
+++ commands/cmd_faction.js
@@ -2,7 +2,6 @@ const data = require('../modules/data');
 const tools = require('../modules/tools');
 
 function getFactionSummary(givenFactionName, factionObject) {
-	var response = "";
 	var factionName = factionObject['name'];
 
 	if (factionName === undefined) {
@@ -10,53 +9,76 @@ function getFactionSummary(givenFactionName, factionObject) {
 	}
 
 	var date = new Date(parseInt(factionObject['lastUpdate'], 10));
-	var niceDate = tools.getEliteDate(date);
+	// var niceDate = tools.getEliteDate(date);
 
-    response += "Data obtained from EDSM at " + niceDate + "\n";
-    response += "**Faction " + factionName + "**:\n";
-
-    response += '```';
-    /*
-	var controllingFactionName = systemData['controllingFaction']
+	// var controllingFactionName = systemData['controllingFaction']
+	var description = "";
+	var content = `Here's data on the **${factionName}** minor faction`;
 	
-	var factionsData = tools.sortByInfluence(systemData['factions']);
-	for (var factionIndex in factionsData) {
-		var factionData = factionsData[factionIndex];
-		var factionName = factionData['name'];
-		var percent = (factionData['influence'] * 100).toFixed(1) + "%";
-		response += (factionName == controllingFactionName) ? "★ " : "◦ "
-		var displayName = factionName.toUpperCase();
-		if (factionData['isPlayer']) {
-			displayName += "†";
-		}		
-		response += (displayName + ": ").padEnd(40 - percent.length);
-		response += percent;
+	// https://leovoel.github.io/embed-visualizer/
+	//response += '```';
+	var embed = {};
+	// var description = "";
+	var systemsData = factionObject['systems']; //tools.sortByInfluence(systemData['factions']);
+	// const indent = '` ` ` ` ` ` ` `'; //'`.' + (" ".repeat(7)) + '.` ';
+	const indent = String.fromCodePoint(0x3000).repeat(7);
+	// const indent = "`." + "\u2063".repeat(5) + " :`";
+
+	for (var systemIndex in systemsData) {
+		var systemData = systemsData[systemIndex];
+		var systemName = ('name' in systemData) ? systemData['name'] : systemIndex;
+		var percent = (systemData['influence'] * 100).toFixed(1) + "%";
+		var displayName = systemName.toUpperCase();
+
+		// description += (factionName == controllingFactionName) ? "\uFF0A " : "\uFF0D "
+		description += '`';
+		description += "∙ "; //(factionName == controllingFactionName) ? "⦿ " : "∙ "
+
+		// description += tools.getMonospacedPercentage(factionData['influence'] * 100, 1, 6) + ": ";
+
+		description += "\u3000".repeat(6 - percent.length) + percent + " :` ";
+		// description += (displayName + ": ").padEnd(40 - percent.length);
+		// description += percent;
 		
-		response += "\n";
+		description += "**" + displayName + "** "
+
+		if (systemData.controllingFaction) {
+			description += "👑";
+		}
+
+		description += "\n";
 		
 		var states = [];
-		states = tools.parseStates(factionData['activeStates']);
+		states = tools.parseStates(systemData['activeStates']);
 		if (states.length > 0) {
-			response += "\xa0\xa0∙ Active:     " + states.join(' ');
-			response += "\n";
+			description += indent + "Active: " + states.join(' ');
+			description += "\n";
 		}
 
-		var recoveringStates = tools.parseStates(factionData['recoveringStates']);
+		var recoveringStates = tools.parseStates(systemData['recoveringStates']);
 		if (recoveringStates.length > 0) {
-			response += "\xa0\xa0∙ Recovering: " + recoveringStates.join(' ');
-			response += "\n";
+			description += indent + "Recovering: " + recoveringStates.join(' ');
+			description += "\n";
 		}
 
-		var pendingStates = tools.parseStates(factionData['pendingStates']);
+		var pendingStates = tools.parseStates(systemData['pendingStates']);
 		if (pendingStates.length > 0) {
-			response += "\xa0\xa0∙ Pending:    " + pendingStates.join(' ');
-			response += "\n";
+			description += indent + "Pending: " + pendingStates.join(' ');
+			description += "\n";
 		}
 	}
-    */
-    response += '```';
+
+	embed.description = description;
+	embed.timestamp = date.toISOString();
+	embed.footer = {
+			"icon_url": "https://cdn.discordapp.com/avatars/" + tools.getMyUserId() + "/" + tools.getMyAvatar() + ".png",
+			"text": "Data obtained by edbot from EDDN"
+	};
 	
-	return response;
+	return {
+		content: content,
+		embed: embed
+	};
 }
 
 module.exports = {
@@ -74,7 +96,7 @@ module.exports = {
 		data.getFaction(factionName).then(function(factionObject) {
 			var response = getFactionSummary(factionName, factionObject);
 	
-			message.channel.send(response);
+			message.channel.send(response.content, { embed: response.embed });
 		}, function(err) {
 			console.log(err);
 		});
