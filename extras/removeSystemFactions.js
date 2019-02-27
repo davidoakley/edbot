0c3b9bc33c8136d9d0e02bc7991b381cc32b8062
davidoakley <david@astraware.com>
2019-02-27 09:43:09 +0000

    Script to remove now-unused systemFaction objects

---
+++ extras/removeSystemFactions.js
@@ -0,0 +1,32 @@
+const redis = require("redis");
+const rejson = require('redis-rejson');
+rejson(redis);
+
+const bluebird = require('bluebird');
+bluebird.promisifyAll(redis);
+
+const redisClient = redis.createClient();
+
+removeSystemFactions().then(function() {
+    console.log("Finished");
+})
+
+async function removeSystemFactions() {
+    var cursor = 0;
+    var delCount = 0;
+    try {
+        do {
+            var result = await redisClient.scanAsync(cursor, 'COUNT', 1000, 'MATCH', 'systemFaction:*'); // eslint-disable-line no-await-in-loop
+            cursor = result.shift();
+            const keyList = result.shift();
+            const count = await redisClient.delAsync(...keyList);
+            delCount += count;
+
+            //systemCount += keyList.length;
+            console.log(`Deleted ${delCount} keys`);
+        } while (cursor != 0);
+    } catch (error) {
+        console.log(error);
+        return 0;
+    }
+}
